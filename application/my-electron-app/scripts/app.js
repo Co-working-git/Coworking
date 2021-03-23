ae715f4fdff4270e8c8453ff9fb5fd9623a518cf
SanderSpaas <80250065+SanderSpaas@users.noreply.github.com>
2021-03-23 17:37:44 +0100

    added some random blocks to test some stuff and made the grid smaller

--- application/my-electron-app/scripts/app.js
+++ application/my-electron-app/scripts/app.js
@@ -1,21 +1,37 @@
 var canvas = new fabric.Canvas('canvas', {
   selection: false
 });
-var grid = 50;
+var grid = 25;
+
 
 // create grid
-
-for (var i = 0; i < (600 / grid); i++) {
-  canvas.add(new fabric.Line([i * grid, 0, i * grid, 600], {
+for (var i = 0; i < (2000 / grid); i++) {
+  canvas.add(new fabric.Line([i * grid, 0, i * grid, canvas.height], {
     stroke: '#ccc',
     selectable: false
   }));
-  canvas.add(new fabric.Line([0, i * grid, 600, i * grid], {
+  canvas.add(new fabric.Line([0, i * grid, canvas.width, i * grid], {
     stroke: '#ccc',
     selectable: false
   }))
 }
-
+//add random objects
+let counter = 0;
+for (var i = 0; i<Math.floor((Math.random() * 500) + 1);i++)
+{
+  canvas.add(new fabric.Rect({
+    left: Math.floor((Math.random() * 500) + 1),
+    top: Math.floor((Math.random() * 500) + 1),
+    width: 100,
+    height: 50,
+    fill: "#" + ((1<<24)*Math.random() | 0).toString(16),
+    originX: 'left',
+    originY: 'top',
+    centeredRotation: true
+  }));
+counter++
+}
+console.log(counter);
 // add objects
 
 canvas.add(new fabric.Rect({
@@ -49,8 +65,8 @@ canvas.add(new fabric.Rect({
   centeredRotation: true
 }));
 canvas.add(new fabric.Rect({
-  left: 100,
-  top: 100,
+  left: 400,
+  top: 500,
   width: 200,
   height: 50,
   fill: 'purple',
@@ -70,7 +86,6 @@ canvas.add(new fabric.Circle({
 }));
 
 // snap to grid
-
 canvas.on('object:moving', function(options) {
   if (Math.round(options.target.left / grid * 4) % 4 == 0 &&
     Math.round(options.target.top / grid * 4) % 4 == 0) {
