ff528693c46476901e3e49342f52704d80967e62
arnoboenders <arno.boenders@student.odisee.be>
2021-05-15 14:05:56 +0200

    Added folder image to choose file Get in js works

--- application/my-electron-app/scripts/index.js
+++ application/my-electron-app/scripts/index.js
@@ -5,7 +5,8 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close = document.getElementsByClassName("close")[0];
+var close1 = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -13,10 +14,12 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close.onclick = function() {
   modal.style.display = "none";
 }
-
+close1.onclick = function() {
+  modal.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
--- application/my-electron-app/scripts/listFiles.js
+++ application/my-electron-app/scripts/listFiles.js
@@ -1,20 +1,48 @@
 "use strict"
-    import fetch from 'electron-fetch'
-    const data = "https://api.airtable.com/v0/app9n5ekQCqfOeyaq/Table%201"
+    const data = "https://api.airtable.com/v0/appV1l6i5krPZ1A9w/Table%201/"
+    let data2;
+    window.addEventListener("load", function(){
+        getProjects();
+    })
 
-    fetch(data, {
-        headers: {
-            'Authorization' : 'Bearer key2Rz6y5n7q7ZLGu'
-        }
-    })
-    .then((response) =>{
-        return (response.json());
-    })
-    .then((records) =>{
-        console.log(records);
-        records.records.forEach(record =>{
-            console.log(record.field.username);
+    function getProjects(){
+        fetch(data, {
+            headers: {
+                'Authorization' : 'Bearer key2Rz6y5n7q7ZLGu'
+            }
         })
-    })
-    .catch((err) => console.log(err));
+        .then((response) =>{
+            return (response.json());
+        })
+        .then((records) =>{
+                    console.log(records);
+                    records.records.forEach(record => {
+                        let post = `<div class="post">
+                                        <h2>Projects babyyyyyy<h2>
+                                        <p>Creator Name: ${record.fields.NameCreator}</p>
+                                        <p>Creation Name: ${record.fields.NameCreation}</p>
+                                        <button id="${record.id}" onclick="getId(this)">Click me for drugs</P>
+                                    </div>`
+                        document.getElementById("projects").innerHTML += post;
+                    })
+            })
+        .catch((err) => console.log(err)); 
+    }
+    function getId(btn){
+        data2 = "https://api.airtable.com/v0/appV1l6i5krPZ1A9w/Table%201/"+btn.id;
+        fetch(data2, {
+            headers: {
+                'Authorization' : 'Bearer key2Rz6y5n7q7ZLGu'
+            }
+        })
+        .then((response) =>{
+            return (response.json());
+        })
+        .then((records) =>{
+                    console.log(records);
+                    let saveData = records.fields.Creations;
+                    canvas.loadFromJSON(saveData);
+                    })
+        .catch((err) => console.log(err));
+    }
     
